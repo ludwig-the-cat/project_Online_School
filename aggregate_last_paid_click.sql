a53bc00070a58fb3caa54aaef009d700ed53a30e
Dmitrii Vorontes <102757651+ludwig-the-cat@users.noreply.github.com>
2024-01-30 19:26:21 +0500

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,88 +1,88 @@
 /*Делаем CTE чтобы агрегировать посетелей по последнему клику*/
-with last_click as (
-    select
+WITH last_click AS (
+    SELECT
         visitor_id,
-        MAX(visit_date) as visit_date
-    from sessions
-    where
-        LOWER(medium) in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-    group by visitor_id
+        MAX(visit_date) AS visit_date
+    FROM sessions
+    WHERE
+        LOWER(medium) IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+    GROUP BY visitor_id
 ),
 /*создаем CTE для агрегации по дате создания, utm_меткам и лидам и их статусу*/ 
-last_paid_click as (
-    select
+last_paid_click AS (
+    SELECT
         l_c.visitor_id,
         l_c.visit_date,
-        s.source as utm_source,
-        s.medium as utm_medium,
-        s.campaign as utm_campaign,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
         l.lead_id,
         l.created_at,
         l.amount,
         l.closing_reason,
         l.status_id
-    from last_click as l_c
-    inner join sessions as s
-        on
+    FROM last_click AS l_c
+    INNER JOIN sessions AS s
+        ON
             l_c.visitor_id = s.visitor_id
-            and l_c.visit_date = s.visit_date
-    left join leads as l
-        on
+            AND l_c.visit_date = s.visit_date
+    LEFT JOIN leads AS l
+        ON
             l_c.visitor_id = l.visitor_id
-            and l_c.visit_date <= l.created_at
+            AND l_c.visit_date <= l.created_at
 ),
 /*агрегация по Utm метакам в рамках компаний VK и Ya*/
-ads as (
-    select
-        TO_CHAR(campaign_date, 'yyyy-mm-dd') as campaign_date,
+ads AS (
+    SELECT
+        TO_CHAR(campaign_date, 'yyyy-mm-dd') AS campaign_date,
         utm_source,
         utm_medium,
         utm_campaign,
-        SUM(daily_spent) as total_cost
-    from vk_ads
-    group by
+        SUM(daily_spent) AS total_cost
+    FROM vk_ads
+    GROUP BY
         TO_CHAR(campaign_date, 'yyyy-mm-dd'),
         utm_source,
         utm_medium,
         utm_campaign
-    union all
-    select
+    UNION ALL
+    SELECT
         TO_CHAR(campaign_date, 'yyyy-mm-dd') as campaign_date,
         utm_source,
         utm_medium,
         utm_campaign,
         SUM(daily_spent) as total_cost
-    from ya_ads
-    group by
+    FROM ya_ads
+    GROUP BY
         TO_CHAR(campaign_date, 'yyyy-mm-dd'),
         utm_source,
         utm_medium,
         utm_campaign
 ),
 
-agg_tab as (
-    select
+agg_tab AS (
+    SELECT
         utm_source,
         utm_medium,
         utm_campaign,
-        TO_CHAR(visit_date, 'yyyy-mm-dd') as visit_date,
-        COUNT(distinct visitor_id) as visitors_count,
-        COUNT(lead_id) as leads_count,
+        TO_CHAR(visit_date, 'yyyy-mm-dd') AS visit_date,
+        COUNT(DISTINCT visitor_id) AS visitors_count,
+        COUNT(lead_id) AS leads_count,
         COUNT(lead_id) filter (
-            where
+            WHERE
             closing_reason = 'Успешно реализовано'
-            or status_id = 142
-        ) as purchases_count,
-        SUM(amount) as revenue
-    from last_paid_click
-    group by
+            OR status_id = 142
+        ) AS purchases_count,
+        SUM(amount) AS revenue
+    FROM last_paid_click
+    GROUP BY
         TO_CHAR(visit_date, 'yyyy-mm-dd'),
         utm_source,
         utm_medium,
         utm_campaign
 )
 
-select
+SELECT
     ag.visit_date,
     ag.visitors_count,
     ag.utm_source,
@@ -92,18 +92,18 @@ select
     ag.leads_count,
     ag.purchases_count,
     ag.revenue
-from agg_tab as ag
-left join ads
-    on
+FROM agg_tab AS ag
+LEFT JOIN ads
+    ON
         ag.visit_date = ads.campaign_date
-        and ag.utm_source = ads.utm_source
-        and ag.utm_medium = ads.utm_medium
-        and ag.utm_campaign = ads.utm_campaign
-order by
-    ag.revenue desc nulls last,
-    ag.visit_date asc,
-    ag.visitors_count desc,
-    ag.utm_source asc,
-    ag.utm_medium asc,
-    ag.utm_campaign asc
-    limit 15;
+        AND ag.utm_source = ads.utm_source
+        AND ag.utm_medium = ads.utm_medium
+        AND ag.utm_campaign = ads.utm_campaign
+ORDER BY
+    ag.revenue DESC nulls LAST,
+    ag.visit_date ASC,
+    ag.visitors_count DESC,
+    ag.utm_source ASC,
+    ag.utm_medium ASC,
+    ag.utm_campaign ASC
+    LIMIT 15;
