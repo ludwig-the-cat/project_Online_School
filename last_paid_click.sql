708f6195d0fd17cd9c761ce72631f9ccc576793c
Dmitrii Vorontes <102757651+ludwig-the-cat@users.noreply.github.com>
2024-01-30 19:40:17 +0500

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -3,22 +3,23 @@
 /* + добавляем row_number в разрезе id пользователей */
 WITH temp AS (
     SELECT
-    s.visitor_id,
-    s.visit_date,
-    s.source AS utm_source,
-    s.medium AS utm_medium,
-    s.campaign AS utm_campaign,
-    l.lead_id, 
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id,
-    row_number() over (partition BY s.visitor_id ORDER BY s.visit_date DESC)
+        s.visitor_id,
+        s.visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        l.lead_id, 
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        row_number() over (PARTITION BY s.visitor_id ORDER BY s.visit_date DESC)
         AS rn
     /* Нумеруем users id, с сортировкой по совершившим последнюю покупку*/
     FROM sessions AS s
     LEFT JOIN leads AS l
-    ON s.visitor_id = l.visitor_id
+        ON 
+            s.visitor_id = l.visitor_id
     AND s.visit_date <= l.created_at
     WHERE s.medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
 )
